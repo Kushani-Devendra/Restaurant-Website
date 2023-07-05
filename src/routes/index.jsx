e8a603694f7a8c755a00cc8fd84adf817dbbcb73
Kushani Sasiprabha <kushani2000@gmail.com>
2023-07-05 12:46:48 +0530

    modified index.jsx file

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,10 +1,10 @@
 export default function Index() {
   return (
     <p id="zero-state">
-      This is a demo for React Router.
-      <br />
+      This is a the Home Page.
+      {/* <br />
       Check out{" "}
-      <a href="https://reactrouter.com">the docs at reactrouter.com</a>.
+      <a href="https://reactrouter.com">the docs at reactrouter.com</a>. */}
     </p>
   );
 }
